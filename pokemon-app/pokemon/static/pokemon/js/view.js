acc1c5ef73241404ec22f81488d08d4143446ac4
CodingDango <babyyodaman2.0@gmail.com>
2025-09-09 20:51:01 +0800

    idk

--- pokemon-app/pokemon/static/pokemon/js/view.js
+++ pokemon-app/pokemon/static/pokemon/js/view.js
@@ -1,11 +1,28 @@
-import { calculateWeaknesses } from './api.js';
-import { capitalize } from './helpers.js'
+import { calculateWeaknesses, fetchPokemonDetails, getPokemonFromLocal } from './api.js';
+import { capitalize, formatId } from './helpers.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const typesDataElement = document.getElementById('typesOfPokemon');
-    const typesJsonString = typesDataElement.textContent;
-    const pokemonTypes = JSON.parse(typesJsonString);
+    loadTypeWeaknesses();
+    loadPrevAndNextNav();
+    formatPokemonIdElement();
+});
 
+function loadDocumentDataElementJson(selector) {
+    const dataElement = document.querySelector(selector);
+
+    if (!dataElement) {
+        console.log(`"${selector}" data element could not be found`);
+        return;
+    }
+
+    const dataString = dataElement.textContent;
+    const dataJson = JSON.parse(dataString);
+
+    return dataJson;
+}
+
+function loadTypeWeaknesses() {
+    const pokemonTypes = loadDocumentDataElementJson('#typesOfPokemon');
     const container = document.getElementById('weaknesses')
     const weaknesses = calculateWeaknesses(pokemonTypes);
 
@@ -16,4 +33,34 @@ document.addEventListener('DOMContentLoaded', () => {
             </span>`
         )
     }
-});
+}
+
+function loadPrevAndNextNav() {
+    const maxPokemonId = 1025;
+    const minPokemonId = 1;
+
+    const id = loadDocumentDataElementJson('#pokemonId');
+    const previousId = ((id - 1) < minPokemonId) ? maxPokemonId : id - 1;
+    const nextId = ((id + 1) > maxPokemonId) ? minPokemonId : id + 1;
+
+    const prevPokemonIdElement = document.getElementById('prev-pokemon-id');
+    prevPokemonIdElement.textContent = `#${formatId(previousId)}`;
+
+    const nextPokemonIdElement = document.getElementById('next-pokemon-id');
+    nextPokemonIdElement.textContent = `#${formatId(nextId)}`;
+
+    document.getElementById('prev-pokemon-name').textContent = capitalize(getPokemonFromLocal(previousId).name);
+    document.getElementById('next-pokemon-name').textContent = capitalize(getPokemonFromLocal(nextId).name);
+
+    prevPokemonIdElement.closest('a').href = `/${previousId}`;
+    nextPokemonIdElement.closest('a').href = `/${nextId}`;
+}
+
+function formatPokemonIdElement() {
+    const pokemonIdElement = document.getElementById('pokemon-id-element')
+    
+    if (!pokemonIdElement) return;
+
+    const formattedIdStr = `#${formatId(parseInt(pokemonIdElement.textContent))}`
+    pokemonIdElement.textContent = formattedIdStr;
+}
